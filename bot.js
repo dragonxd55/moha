31a6e8f654bcc7b54c7db4964d3aac50a3bbc61d
dragonxd55 <37715638+dragonxd55@users.noreply.github.com>
2018-11-13 01:38:23 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('guildMemberAdd', member => {
+      if(member.guild.id !== '511543836053471254') return;
+    setTimeout(function() {
+    member.guild.channels.find(r => r.id === '511550650723598373').send('**Bienvenido Old Servidor** :coffee: :dizzy: ,');
+},3000);
+});
+client.login(process.env.BOT_TOKEN);
